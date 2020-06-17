f7f91751091767215d548b2beacd18e3f0a93599
Duk2123 <paweldyczek02@gmail.com>
2020-06-17 13:21:13 +0200

    PartyCreate pop-up added

--- src/components/pop-ups/PartyCreate/PartyCreate.jsx
+++ src/components/pop-ups/PartyCreate/PartyCreate.jsx
@@ -8,13 +8,51 @@ import { connect } from 'react-redux';
 import './partyCreate.sass';
 
 class PartyCreate extends React.Component {
-  state = {};
+  state = {
+    today: new Date(),
+    dd: 0,
+    mm: 0,
+    yyyy: 0
+  };
+  date = () => {
+    const state = { ...this.state };
+
+    state.dd = state.today.getDate();
+    state.mm = state.today.getMonth() + 1;
+    state.yyyy = state.today.getFullYear();
+
+    this.setState({ ...state });
+  };
+
+  componentDidMount = () => {
+    this.date();
+  };
 
   render() {
-    const {} = this.state;
     return (
       <>
-        <div className="partyCreate"></div>
+        <div className="partyCreate">
+          <div className="partyCreate__container">
+            <h1 className="partyCreate__h1">Create your party</h1>
+            <div className="partyCreate__inputContainer">
+              <p className="partyCreate__p">Party name</p>
+              <input className="partyCreate__input" type="text" />
+            </div>
+            <div className="partyCreate__inputContainer">
+              <p className="partyCreate__p">Party start date</p>
+              <input
+                className="partyCreate__input"
+                type="date"
+                min={`${this.state.yyyy}-${this.state.mm}-${this.state.dd}`}
+                max={`${this.state.yyyy + 1}-${this.state.mm}-${this.state.dd}`}
+              />
+            </div>
+            <div className="partyCreate__buttons">
+              <button className="partyCreate__button">Save</button>
+              <button className="partyCreate__button">Cancel</button>
+            </div>
+          </div>
+        </div>
       </>
     );
   }
